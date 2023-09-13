02aa9feef22fa786dcbd166c9bd2906eab0671ba
An Nguyen <an6122003@gmail.com>
2023-09-13 19:58:37 +0700

    initial commit to create xcode file

---
+++ TinFood/TinFoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinFoodApp.swift
+//  TinFood
+//
+//  Created by An on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TinFoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
