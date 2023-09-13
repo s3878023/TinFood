1906344d95bbbd53b7dcc5e831e0f5221cb4488c
An Nguyen <an6122003@gmail.com>
2023-09-14 00:24:15 +0700

    Add firebase package and initialization code

--- TinFood/TinFoodApp.swift
+++ TinFood/TinFoodApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TinFoodApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
