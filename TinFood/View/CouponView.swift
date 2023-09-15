5dc04a0b65d608b5291f496eb0ccb6c599bac4d4
Quan Tong <61928209+nhatquantong@users.noreply.github.com>
2023-09-15 17:45:16 +0700

    commit

---
+++ TinFood/View/CouponView.swift
@@ -0,0 +1,20 @@
+//
+//  CouponView.swift
+//  TinFood
+//
+//  Created by Nhật Quân on 15/09/2023.
+//
+
+import SwiftUI
+
+struct CouponView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CouponView_Previews: PreviewProvider {
+    static var previews: some View {
+        CouponView()
+    }
+}
