fe900c65a7ecb7ac42b470d5e971cf33a957ffb5
Nguyen Duc Dai <s3878023@rmit.edu.vn>
2023-09-24 21:16:11 +0700

    adding the sort and search function to the shop

--- TinFood/Views/Swipe/LikedShop.swift
+++ TinFood/Views/Swipe/LikedShop.swift
@@ -6,53 +6,136 @@
 //
 
 
-//struct LikedShop: View {
-//    @ObservedObject var viewModel: homeViewModel
 //
-//     var body: some View {
-//         NavigationView {
-//             List(viewModel.likedShops) { shop in
-//                 // Display each liked shop as a list item
-//                 NavigationLink(destination: MerchantViewForUser()) {
-//                      Text(shop.storename)
-//                     .font(.system(size: 16))
-//                 }
-//             }
-//             .navigationBarTitle("Liked Shops")
-//             .onAppear {
-//                 viewModel.fetchLikedShops()
-//             }
-//         }
-//     }
+//import SwiftUI
+//struct LikedShop: View {
+//    @ObservedObject var homeData: homeViewModel
+//    @State private var searchText = ""
+//
+//    init(homeData: homeViewModel) {
+//        self.homeData = homeData
+//        homeData.fetchLikedShops() // Fetch the liked shops when the view is initialized
+//    }
+//
+//    var body: some View {
+//        ScrollView {
+//            VStack {
+//                Text("Liked Shops")
+//                    .font(.title)
+//                    .padding(.top, 20) // Add padding to create space above the title
+//                TextField("Search by Store Name", text: $searchText)
+//                    .textFieldStyle(RoundedBorderTextFieldStyle())
+//                    .padding(.horizontal, 15)
+//                    .padding(.bottom, 10)
+//
+//                ForEach(filteredLikedShops.indices, id: \.self) { index in
+//                    // Use filteredLikedShops instead of homeData.likedShops
+//                    let shop = filteredLikedShops[index]
+//
+//                    // Display each liked shop as a list item with fixed size
+//                    HStack(alignment: .top, spacing: 10) {
+//                        // Use AsyncImage to load the shop.image asynchronously
+//                        AsyncImage(url: URL(string: shop.image)) { image in
+//                            image.resizable()
+//                                .aspectRatio(contentMode: .fill)
+//                                .frame(width: 100, height: 100)
+//                                .cornerRadius(15)
+//                        } placeholder: {
+//                            ProgressView()
+//                        }
+//                        .frame(width: 100, height: 100)
+//
+//                        VStack(alignment: .leading, spacing: 5) {
+//                            Text(shop.storename)
+//                                .font(.system(size: 16))
+//                                .frame(maxWidth: .infinity, alignment: .leading)
+//                            Text(shop.address)
+//                                .font(.subheadline)
+//                                .foregroundColor(.gray)
+//                                .frame(maxWidth: .infinity, alignment: .leading)
+//                        }
+//                    }
+//                    .padding(.horizontal, 15)
+//                    .background(Color.gray.opacity(0.2))
+//                    .cornerRadius(15)
+//
+//                    // Add spacing between shop items
+//                    if index != filteredLikedShops.count - 1 {
+//                        Spacer().frame(height: 20)
+//                    }
+//                }
+//            }
+//            .padding()
+//        }
+//    }
+//
+//    var filteredLikedShops: [Shop] {
+//        if searchText.isEmpty {
+//            return homeData.likedShops
+//        } else {
+//            return homeData.likedShops.filter { shop in
+//                return shop.storename.localizedCaseInsensitiveContains(searchText)
+//            }
+//        }
+//    }
 //}
-
+//struct LikedShop_Previews: PreviewProvider {
+//    static var previews: some View {
+//        // Initialize a homeViewModel and pass it to LikedShop
+//        LikedShop(homeData: homeViewModel())
+//    }
+//}
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 import SwiftUI
 
 struct LikedShop: View {
     @ObservedObject var homeData: homeViewModel
     @State private var searchText = ""
+    @State private var isSortingAlphabetically = false
+    @State private var originalLikedShops: [Shop]?
+
     init(homeData: homeViewModel) {
         self.homeData = homeData
-        homeData.fetchLikedShops() // Fetch the liked shops when the view is initialized
+        homeData.fetchLikedShops()
     }
 
     var body: some View {
         ScrollView {
             VStack {
-                Text("Liked Shops")
-                    .font(.title)
-                    .padding(.top, 20) // Add padding to create space above the title
-                TextField("Search by Store Name", text: $searchText)
-                                   .textFieldStyle(RoundedBorderTextFieldStyle())
-                                   .padding(.horizontal, 15)
-                                   .padding(.bottom, 10)
+                HStack {
+                    Text("Liked Shops")
+                        .font(.title)
+                        .padding(.top, 20)
 
-                ForEach(homeData.likedShops.indices, id: \.self) { index in
-                    
-                    // Display each liked shop as a list item with fixed size
+                    Button(action: {
+                        isSortingAlphabetically.toggle()
+                        if isSortingAlphabetically {
+                            if originalLikedShops == nil {
+                                originalLikedShops = homeData.likedShops
+                            }
+                            homeData.likedShops.sort { $0.storename < $1.storename }
+                        } else {
+                            if let originalShops = originalLikedShops {
+                                homeData.likedShops = originalShops
+                            }
+                            originalLikedShops = nil
+                        }
+                    }) {
+                        Image(systemName: isSortingAlphabetically ? "person.fill" : "person.fill")
+                            .font(.title)
+                    }
+                }
+
+                TextField("Search by Store Name", text: $searchText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal, 15)
+                    .padding(.bottom, 10)
+
+                ForEach(filteredLikedShops.indices, id: \.self) { index in
+                    let shop = filteredLikedShops[index]
+
                     HStack(alignment: .top, spacing: 10) {
-                        // Use AsyncImage to load the shop.image asynchronously
-                        AsyncImage(url: URL(string: homeData.likedShops[index].image)) { image in
+                        AsyncImage(url: URL(string: shop.image)) { image in
                             image.resizable()
                                 .aspectRatio(contentMode: .fill)
                                 .frame(width: 100, height: 100)
@@ -63,22 +146,20 @@ struct LikedShop: View {
                         .frame(width: 100, height: 100)
 
                         VStack(alignment: .leading, spacing: 5) {
-                            Text(homeData.likedShops[index].storename)
+                            Text(shop.storename)
                                 .font(.system(size: 16))
                                 .frame(maxWidth: .infinity, alignment: .leading)
-                            Text(homeData.likedShops[index].address)
+                            Text(shop.address)
                                 .font(.subheadline)
                                 .foregroundColor(.gray)
                                 .frame(maxWidth: .infinity, alignment: .leading)
-
                         }
                     }
                     .padding(.horizontal, 15)
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(15)
 
-                    // Add spacing between shop items
-                    if index != homeData.likedShops.count - 1 {
+                    if index != filteredLikedShops.count - 1 {
                         Spacer().frame(height: 20)
                     }
                 }
@@ -86,15 +167,16 @@ struct LikedShop: View {
             .padding()
         }
     }
+
     var filteredLikedShops: [Shop] {
-          if searchText.isEmpty {
-              return homeData.likedShops
-          } else {
-              return homeData.likedShops.filter { shop in
-                  return shop.storename.localizedCaseInsensitiveContains(searchText)
-              }
-          }
-      }
+        if searchText.isEmpty {
+            return homeData.likedShops
+        } else {
+            return homeData.likedShops.filter { shop in
+                return shop.storename.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
 }
 
 struct LikedShop_Previews: PreviewProvider {
@@ -103,123 +185,3 @@ struct LikedShop_Previews: PreviewProvider {
         LikedShop(homeData: homeViewModel())
     }
 }
-
-//struct LikedShop: View {
-////    @ObservedObject var viewModel: homeViewModel
-//    @ObservedObject var homeData: homeViewModel
-//
-//    init(homeData: homeViewModel) {
-//        self.homeData = homeData
-//        homeData.fetchLikedShops() // Fetch the liked shops when the view is initialized
-//    }
-//
-//    var body: some View {
-//
-//            List(homeData.likedShops) { shop in
-//                // Display each liked shop as a list item
-//                    Text(shop.storename)
-//                        .font(.system(size: 16))
-//                Text(shop.address)
-//                Text(shop.image)
-//            }
-//
-//    }
-//}
-//
-//struct LikedShop_Previews: PreviewProvider {
-//    static var previews: some View {
-//        // Initialize a homeViewModel and pass it to LikedShop
-//        LikedShop(homeData:homeViewModel())
-//    }
-//}
-
-
-
-//import SwiftUI
-//
-//struct LikedShop: View {
-//    @ObservedObject var viewModel: homeViewModel
-//    @State private var filteredShops: [Shop] = []
-//
-//    var body: some View {
-//        NavigationView {
-//            List(filteredShops) { shop in
-//                // Display each liked shop as a list item
-//                NavigationLink(destination: MerchantViewForUser()) {
-//                    Text(shop.storename)
-//                        .font(.system(size: 16))
-//                }
-//            }
-//            .navigationBarTitle("Liked Shops")
-//            .onAppear {
-//                filterShops()
-//            }
-//        }
-//    }
-//
-//    func filterShops() {
-//        // Get the unique store IDs from the liked shops
-//        let uniqueStoreIDs = Set(viewModel.likedShops.map { $0.id })
-//
-//        // Filter out shops that have IDs not in the uniqueStoreIDs set
-//        filteredShops = viewModel.likedShops.filter { uniqueStoreIDs.contains($0.id) }
-//    }
-//}
-//
-//struct LikedShop_Previews: PreviewProvider {
-//    static var previews: some View {
-//        // Initialize a homeViewModel and pass it to LikedShop
-//        LikedShop(viewModel: homeViewModel())
-//    }
-//}
-
-
-
-//import SwiftUI
-//
-//struct LikedShop: View {
-//    @ObservedObject var viewModel: homeViewModel
-//    @State private var filteredShops: [Shop] = []
-//
-//    var body: some View {
-//        NavigationView {
-//            List(filteredShops) { shop in
-//                // Display each liked shop as a list item
-//                NavigationLink(destination: MerchantViewForUser()) {
-//                    Text(shop.storename)
-//                        .font(.system(size: 16))
-//                }
-//            }
-//            .navigationBarTitle("Liked Shops")
-//            .onAppear {
-//                filterShops()
-//            }
-//        }
-//    }
-//
-//    func filterShops() {
-//        // Create a set to keep track of unique store IDs
-//        var uniqueStoreIDs: Set<String> = []
-//
-//        // Create an array to hold the filtered shops
-//        var filteredShops: [Shop] = []
-//
-//        for shop in viewModel.likedShops {
-//            if !uniqueStoreIDs.contains(shop.id) {
-//                // If the store ID is not in the set, it's unique, so add it to the filtered shops
-//                uniqueStoreIDs.insert(shop.id)
-//                filteredShops.append(shop)
-//            }
-//        }
-//
-//        // Update the @State variable with the filtered shops
-//        self.filteredShops = filteredShops
-//    }
-//}
-//
-//struct LikedShop_Previews: PreviewProvider {
-//    static var previews: some View {
-//        // Initialize a homeViewModel and pass it to LikedShop
-//        LikedShop(viewModel: homeViewModel())
-//    }
-//}
--- TinFood/Views/Swipe/homeViewModel.swift
+++ TinFood/Views/Swipe/homeViewModel.swift
@@ -19,7 +19,83 @@ class homeViewModel: ObservableObject {
     init() {
         fetchShopData() // Fetch shop data from Firestore
     }
+
+    func fetchShopData() {
+        db.collection("Shops").getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error fetching data: \(error.localizedDescription)")
+            } else {
+                if let snapshot = snapshot {
+                    self.fetch_shops = snapshot.documents.compactMap { document in
+                        let data = document.data()
+                        _ = document.documentID
+                        return Shop(
+                            id : document.documentID,
+//                            id: data["id"] as? String ?? "",
+                            storename: data["storename"] as? String ?? "",
+                            address: data["address"] as? String ?? "",
+                            image: data["image"] as? String ?? ""
+                        )
+                    }
+                    self.displaying_shops = self.fetch_shops
+                }
+            }
+        }
+    }
+
+    func getIndex(shop: Shop) -> Int {
+        let index = displaying_shops?.firstIndex { currentShop in
+            return shop.id == currentShop.id
+        } ?? 0
+        return index
+    }
+
+    func fetchLikedShops() {
+        let currentUser = Auth.auth().currentUser
+        let userUID = currentUser?.uid
+        
+        guard let validUserUID = userUID else {
+            print("User is not logged in")
+            return
+        }
+
+        let userRef = db.collection("User").document(validUserUID)
+
+        userRef.getDocument { (snapshot, error) in
+            if let error = error {
+                print("Error fetching user document: \(error.localizedDescription)")
+            } else {
+                if let data = snapshot?.data(),
+                   let likedShopIds = data["likedShops"] as? [String] {
+                    // Fetch liked shops based on the shop IDs
+                    let likedShopRefs = likedShopIds.map { self.db.collection("Shops").document($0) }
+                    self.fetchShops(from: likedShopRefs)
+                }
+            }
+        }
+    }
+
+    private func fetchShops(from shopRefs: [DocumentReference]) {
+        for shopRef in shopRefs {
+            shopRef.getDocument { (snapshot, error) in
+                if let error = error {
+                    print("Error fetching liked shop document: \(error.localizedDescription)")
+                } else if let data = snapshot?.data() {
+                    // Parse the shop data and add it to the likedShops array
+                    let likedShop = Shop(
+                        id: snapshot!.documentID,
+                        storename: data["storename"] as? String ?? "",
+                        address: data["address"] as? String ?? "",
+                        image: data["image"] as? String ?? ""
+                    )
+                    self.likedShops.append(likedShop)
+                }
+            }
+        }
+    }
     
+}
+
 //    func fetchShopData(completion: @escaping ([Shop]) -> Void) {
 //        db.collection("Shops").getDocuments { (snapshot, error) in
 //            if let error = error {
@@ -90,112 +166,4 @@ class homeViewModel: ObservableObject {
 //            }
 //        }
 //    }
-    
-   
-    func fetchShopData() {
-        db.collection("Shops").getDocuments { (snapshot, error) in
-            if let error = error {
-                print("Error fetching data: \(error.localizedDescription)")
-            } else {
-                if let snapshot = snapshot {
-                    self.fetch_shops = snapshot.documents.compactMap { document in
-                        let data = document.data()
-                        _ = document.documentID
-                        return Shop(
-                            id : document.documentID,
-//                            id: data["id"] as? String ?? "",
-                            storename: data["storename"] as? String ?? "",
-                            address: data["address"] as? String ?? "",
-                            image: data["image"] as? String ?? ""
-                        )
-                    }
-                    self.displaying_shops = self.fetch_shops
-                }
-            }
-        }
-    }
 
-    func getIndex(shop: Shop) -> Int {
-        let index = displaying_shops?.firstIndex { currentShop in
-            return shop.id == currentShop.id
-        } ?? 0
-        return index
-    }
-    
-//    func fetchLikedShops() {
-//            // Create a reference to the "Shops" collection in Firestore
-//            let shopsRef = db.collection("Shops")
-//
-//            // Create a query to fetch shops where the shop ID is in the likedShops array
-//        shopsRef.whereField(FieldPath.documentID(), in: likedShops).getDocuments { (snapshot, error) in
-//                if let error = error {
-//                    print("Error fetching liked shops: \(error.localizedDescription)")
-//                } else {
-//                    if let snapshot = snapshot {
-//                        let likedShops = snapshot.documents.compactMap { document in
-//                            let data = document.data()
-//                            return Shop(
-////                                id: document.documentID,
-//                                storename: data["storename"] as? String ?? "",
-//                                address: data["address"] as? String ?? "",
-//                                image: data["image"] as? String ?? ""
-//                            )
-//                        }
-//
-//                        // You can now use the `likedShops` array to access the liked shops
-//                        print("Liked Shops: \(likedShops)")
-//                    }
-//                }
-//            }
-//        }
-    func fetchLikedShops() {
-        let currentUser = Auth.auth().currentUser
-        let userUID = currentUser?.uid
-        
-        guard let validUserUID = userUID else {
-            print("User is not logged in")
-            return
-        }
-
-        let userRef = db.collection("User").document(validUserUID)
-
-        userRef.getDocument { (snapshot, error) in
-            if let error = error {
-                print("Error fetching user document: \(error.localizedDescription)")
-            } else {
-                if let data = snapshot?.data(),
-                   let likedShopIds = data["likedShops"] as? [String] {
-                    // Fetch liked shops based on the shop IDs
-                    let likedShopRefs = likedShopIds.map { self.db.collection("Shops").document($0) }
-                    self.fetchShops(from: likedShopRefs)
-                }
-            }
-        }
-    }
-
-    private func fetchShops(from shopRefs: [DocumentReference]) {
-        // Fetch the liked shops from Firestore based on the DocumentReferences
-        // You can use a similar approach as in your homeViewModel's fetchShopData
-        // to fetch and populate the likedShops array
-        // ...
-
-        // Example code for fetching liked shops
-        for shopRef in shopRefs {
-            shopRef.getDocument { (snapshot, error) in
-                if let error = error {
-                    print("Error fetching liked shop document: \(error.localizedDescription)")
-                } else if let data = snapshot?.data() {
-                    // Parse the shop data and add it to the likedShops array
-                    let likedShop = Shop(
-                        id: snapshot!.documentID,
-                        storename: data["storename"] as? String ?? "",
-                        address: data["address"] as? String ?? "",
-                        image: data["image"] as? String ?? ""
-                    )
-                    self.likedShops.append(likedShop)
-                }
-            }
-        }
-    }
-    
-}
